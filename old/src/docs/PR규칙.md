d48a7fe2a313159a7e7d05347128bace3c0332e1
hkjeon13 <hkjeon13@gmail.com>
2021-04-13 17:41:03 +0900

    test

---
+++ old/src/docs/PR규칙.md
@@ -0,0 +1,32 @@
+# Pull Request  규칙 정하기
+
+### commit 규칙 정하기?
+
+
+
+### branch 규칙 정하기?
+
+브랜치 
+
+
+
+### 간단 PR 튜토리얼
+
+https://github.com/hishamhm/pull-request-tutorial
+
+
+
+### rebase 사용해보기?
+
+일단 merge 로만 작업해볼까 엄청 많이 커밋하지도 않을 것 같음
+
+
+
+### conflict 줄이려면 동시 프로그래밍 도 좋음
+
+vscode live programming
+
+pycharm floobits 
+
+근데 막상 하려면 귀찮음
+
---
+++ old/test2/test_naver_review.py
@@ -0,0 +1,77 @@
+from old.ktextaug import BackTranslate
+from old.ktextaug import tokenize, random_swap
+from tqdm import trange
+import pandas as pd
+
+def main(file_path='review_total.csv', save_path=None):
+    translator = BackTranslate()
+
+    df = pd.read_csv(file_path, index_col=False)
+    # print(df.head())
+    #
+    # total = 0
+    #
+    # for i in range(len(df)):
+    #     try:
+    #         if '|' in df.iloc[i, 1]:
+    #             print(df.iloc[i, 1])
+    #         total += 1
+    #         # total += len(df.iloc[i, 1])
+    #     except Exception as e:
+    #         print(e)
+    #         print(df.iloc[i, 1])
+    # print(total)
+    #
+    # error = 0
+    row_list_org = []
+    for i in trange(len(df), desc="Tokenization"):
+        try:
+            tmp = {'review': '|'.join(tokenize(df.iloc[i, 1])),
+                    'label': df.iloc[i, 2]}
+            row_list_org.append(tmp)
+        except Exception as e:
+            print(f"Index : {i}")
+            print(e)
+            print(df.iloc[i, 1])
+            # error += 1
+            continue
+    df_org = pd.DataFrame(row_list_org)
+    # print(error)
+
+    row_list_bt = []
+    # for i in trange(len(df), desc="Backtranslation"):
+    for i in trange(len(df), desc="Backtranslation"):
+        try:
+            review = '|'.join(tokenize(translator.backtranslate(df.iloc[i, 1])))
+            tmp = {'review': review,
+                   'label': df.iloc[i, 2]}
+            row_list_bt.append(tmp)
+        except Exception as e:
+            print(f"Index : {i}")
+            print(e)
+            print(df.iloc[i, 1])
+            continue
+    df_bt = pd.DataFrame(row_list_bt)
+
+
+    row_list_rs = []
+    for i in trange(len(df), desc="Random_swap"):
+        try:
+            tok_words = tokenize(df.iloc[i, 1])
+            tmp = {'review': '|'.join(random_swap(tok_words, int(0.2 * len(tok_words)))),
+                   'label': df.iloc[i, 2]}
+            row_list_rs.append(tmp)
+        except Exception as e:
+            print(f"Index : {i}")
+            print(e)
+            print(df.iloc[i, 1])
+            continue
+    df_rs = pd.DataFrame(row_list_rs)
+
+    result = df_org.append(df_bt, ignore_index=True)
+    result = result.append(df_rs, ignore_index=True)
+    result.to_csv("./src/data/aug_tok_train_s1000.csv")
+
+if __name__ == '__main__':
+    path = "./src/data/ratings_train_s1000.csv"
+    main(file_path=path)
