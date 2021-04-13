0af49c3694fbaedda624d81d1290a8b879406169
hkjeon13 <hkjeon13@gmail.com>
2021-04-13 19:37:36 +0900

    test

--- lib/augmentations/synonym_replacement.py
+++ lib/augmentations/synonym_replacement.py
@@ -1,16 +1,12 @@
-import random
-from .utils import isStopword, isWord, get_synonym
+import requests
+from bs4 import BeautifulSoup
 
 
-def synonym_replace(corpus, prob, tokenizer, rng, **kwargs):
-    if isinstance(corpus, str):
-        corpus = tokenizer.tokenize(corpus)
-
-
-def synonym_replace(corpus, tokenizer, n_rep, **kwargs):
+def synonym_replace(corpus, tokenizer,rng, n_rep,**kwargs):
     # check if there is a punctuation mark
     if isinstance(corpus, str):
         corpus = tokenizer.tokenize(corpus)
+
     punctuations = [".", ",", ":", ";", "?", "!"]
     if corpus[-1] in punctuations:
         keep = corpus[-1]
@@ -21,12 +17,12 @@ def synonym_replace(corpus, tokenizer, n_rep, **kwargs):
 
     result = words[:]
     nonStop = [w for w in result if (not isStopword(w)) and isWord(w)]
-    random.shuffle(nonStop)
+    rng.shuffle(nonStop)
     num_replacement = 0
     for random_word in nonStop:
-        synonym = get_synonym(random_word)
+        synonym = get_synonym(random_word, rng)
         if len(synonym) >= 1:
-            synonym = random.choice(list(synonym))
+            synonym = rng.choice(list(synonym))
             new_word = [synonym if word == random_word else word for word in result]
             num_replacement += 1
         if num_replacement >= n_rep:
@@ -35,3 +31,41 @@ def synonym_replace(corpus, tokenizer, n_rep, **kwargs):
     sentence = " ".join(new_word)
     result = sentence.split(" ")
     return result + [keep]
+
+
+def define_stopwords(new_stopwords: list) -> list:
+    """
+    define new stopwords list
+    :param stopwords: list of string.
+    :return:
+    """
+    global stopwords
+    stopwords = new_stopwords
+    print("stopwords list has been updated.")
+
+
+def isWord(word):
+    return word.isalnum()
+
+
+def isStopword(word):
+    if word in stopwords:
+        return True
+    else:
+        return False
+
+
+def get_synonym(word, rng):
+    relate_list = []
+    res = requests.get("https://dic.daum.net/search.do?q=" + word)
+    soup = BeautifulSoup(res.content, "html.parser")
+    try:
+        word_id = soup.find("ul", class_="list_relate")
+    except AttributeError:
+        return word
+    if word_id == None:
+        return word
+    for tag in word_id.find_all("a"):
+        relate_list.append(tag.text)
+
+    return rng.choice(relate_list)
