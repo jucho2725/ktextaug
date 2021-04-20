f520637d2ff9173c6b826d1f88c1218a3cdc319b
jucho2725 <cju2725@gmail.com>
2021-04-20 15:47:04 +0900

    0.1.9c1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ktextaug",
-    version="0.1.9c",
+    version="0.1.9c1",
     description="data augmentation tool for Korean",
     author="jinuk.cho, eddie.jeon, jonghyeok.park, junghoon.lee, minsu.jeong. all from ING Lab, SKKU.",
     author_email="cju2725@gmail.com",
